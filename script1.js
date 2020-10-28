4376f40ed4baaf5326042a78a9042672fb2a7a17
north4web <69797932+north4web@users.noreply.github.com>
2020-10-28 08:18:32 +0100

    first commit

---
+++ script1.js
@@ -0,0 +1,45 @@
+function getImage(event) {
+    // event.preventDefault();
+
+    let img = document.createElement("img");
+    
+    let imageFile1 = "images/purple.png";
+    let imageFile2 = "images/blue.png";
+    let imageFile3 = "images/orange.png";
+       
+
+    let randomNumber = Math.floor((Math.random()) * 10);
+
+    let imageFile; 
+
+    if (randomNumber < 4) { 
+        imageFile = imageFile1;
+    }
+    else if (randomNumber < 7) {
+        imageFile = imageFile2;
+    }
+    else {
+        imageFile = imageFile3;
+    }
+
+    // img.setAttribute("src", "images/purple.jpg");
+         img.setAttribute("src", imageFile);
+         img.setAttribute("width", "150");
+         img.setAttribute("height", "200");
+
+    let box = document.getElementById("box");
+
+    box.appendChild(img);
+}
+
+function removeImage(event) {
+    // let box = document.getElementById("box");
+    box.removeChild(box.childNodes[0]);
+}
+
+function removeAll(event) {
+    event.preventDefault()
+    let box = document.getElementById("box");
+    box.innerHTML="";
+
+}
