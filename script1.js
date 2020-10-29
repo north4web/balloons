5d041acef27f8c8947da8c5e77d14c8e7a94c597
north4web <69797932+north4web@users.noreply.github.com>
2020-10-29 11:08:19 +0100

    refactored code using concatenation to create file names

--- script1.js
+++ script1.js
@@ -2,30 +2,33 @@ function getImage(event) {
     // event.preventDefault();
 
     let img = document.createElement("img");
+//all lines below in one line 
+    img.setAttribute("src", "images/img" + (Math.round((Math.random()) * 2)) + ".png");
     
-    let imageFile1 = "images/purple.png";
-    let imageFile2 = "images/blue.png";
-    let imageFile3 = "images/orange.png";
+//long way: 
+    // let imageFile1 = "images/img0.png";
+    // let imageFile2 = "images/img1.png";
+    // let imageFile3 = "images/img2.png";
        
 
-    let randomNumber = Math.floor((Math.random()) * 10);
+    // let randomNumber = Math.floor((Math.random()) * 10);
 
-    let imageFile; 
+    // let imageFile; 
 
-    if (randomNumber < 4) { 
-        imageFile = imageFile1;
-    }
-    else if (randomNumber < 7) {
-        imageFile = imageFile2;
-    }
-    else {
-        imageFile = imageFile3;
-    }
+    // if (randomNumber < 4) { 
+    //     imageFile = imageFile1;
+    // }
+    // else if (randomNumber < 7) {
+    //     imageFile = imageFile2;
+    // }
+    // else {
+    //     imageFile = imageFile3;
+    // }
 
-    // img.setAttribute("src", "images/purple.jpg");
-         img.setAttribute("src", imageFile);
-         img.setAttribute("width", "150");
-         img.setAttribute("height", "200");
+        // // img.setAttribute("src", "images/purple.jpg");
+        //      img.setAttribute("src", imageFile);
+        //      img.setAttribute("width", "150");
+        //      img.setAttribute("height", "200");
 
     let box = document.getElementById("box");
 
