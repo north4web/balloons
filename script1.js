59a7fbf43704d16fd840b9c50aeeaf9b2728589a
north4web <69797932+north4web@users.noreply.github.com>
2020-11-04 17:00:22 +0100

    changed random to be from 1-3

--- script1.js
+++ script1.js
@@ -1,12 +1,39 @@
+
+function mode() {
+let myStyle = document.querySelector("input[name='style']:checked").value;
+
+
+    let flexStyle = `
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap`;
+    let blockStyle = `
+        display: block`;
+
+
+        if (myStyle === "flex") {
+            document.getElementById("box").style = flexStyle;
+        }
+        else {
+            document.getElementById("box").style = blockStyle;
+        }
+    }
+
 function getImage(event) {
     // event.preventDefault();
 
+    mode();
     let img = document.createElement("img");
+    let randomNumber = (Math.floor((Math.random() * 3) + 1))
 //all lines below in one line 
-    img.setAttribute("src", "images/img" + (Math.round((Math.random()) * 2)) + ".png");
+    img.setAttribute("src", "images/img" + randomNumber + ".png");
     
+
+    // console.log(randomNumber);
+
+
 //long way: 
-    // let imageFile1 = "images/img0.png";
+    // let imageFile1 = "images/img3.png";
     // let imageFile2 = "images/img1.png";
     // let imageFile3 = "images/img2.png";
        
@@ -30,13 +57,17 @@ function getImage(event) {
         //      img.setAttribute("width", "150");
         //      img.setAttribute("height", "200");
 
+    
     let box = document.getElementById("box");
-
     box.appendChild(img);
+
+    
+
 }
 
 function removeImage(event) {
     // let box = document.getElementById("box");
+    mode();
     box.removeChild(box.childNodes[0]);
 }
 
